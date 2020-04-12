d33db1db8143c3bdcf9fe31aa17086961dd674d2
Tim <github@timgreen.ws>
2020-04-12 20:24:31 +1000

    added search function

--- searchform.php
+++ searchform.php
@@ -3,16 +3,14 @@
  * The template for displaying search forms
  */
 ?>
-<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<!--label for="s" class="assistive-text"><?php _e( 'Search', 'my-theme' ); ?></label-->
-	<div class="row">
-		<div class="col-md-6">
-			<div class="input-group">
-				<input type="text" name="s" id="s" class="form-control" placeholder="<?php _e( 'Search', 'my-theme' ); ?>" />
-				<span class="input-group-btn">
-					<button type="submit" class="btn btn-secondary" name="submit" id="searchsubmit"><?php _e( 'Search', 'my-theme' ); ?></button>
-				</span>
-			</div><!-- /.input-group -->
-		</div><!-- /.col -->
-	</div><!-- /.row -->
-</form>
+<div class="search">
+	<div class="search-form">
+		<form method="get" id="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+        <input type="search" placeholder="Search here..." name="search" />
+            <button type="submit">
+              <span><i class="fa fa-search"></i></span>
+            </button>
+		</form>
+	</div>
+</div>
+  <!--Search Form-->
