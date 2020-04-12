171063b4a10544ad75df7ed33f1900b695962486
Tim <github@timgreen.ws>
2020-04-12 20:20:07 +1000

    removed unnecessary default functions for uptake theme

--- header.php
+++ header.php
@@ -2,16 +2,13 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
 	<?php wp_head(); ?>
 </head>
 
 <?php
 	$navbar_scheme = get_theme_mod( 'navbar_scheme', 'navbar-light bg-light' ); // get custom meta-value
 	$navbar_position = get_theme_mod( 'navbar_position', 'static' ); // get custom meta-value
-
 	$search_enabled = get_theme_mod( 'search_enabled', '1' ); // get custom meta-value
 ?>
 
@@ -40,31 +37,7 @@
 					?>
 				</a>
 
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="<?php _e( 'Toggle navigation', 'my-theme' ); ?>">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-				
-				<div id="navbar" class="collapse navbar-collapse">
-					<?php
-						/** Loading WordPress Custom Menu (theme_location) **/
-						wp_nav_menu( array(
-							'theme_location'  => 'main-menu',
-							'container'       => '',
-							'menu_class'      => 'navbar-nav',
-							'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
-							'walker'          => new WP_Bootstrap_Navwalker(),
-						) );
-					?>
-					
-					<?php if ( '1' === $search_enabled ) : ?>
-						<form class="form-inline search-form my-2 my-lg-0" role="search" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-							<input type="text" id="s" name="s" class="form-control mr-sm-2" placeholder="<?php _e( 'Search', 'my-theme' ); ?>...">
-							<button type="submit" id="searchsubmit" name="submit" class="btn btn-outline-secondary my-2 my-sm-0"><?php _e( 'Search', 'my-theme' ); ?></button>
-						</form>
 					<?php endif; ?>
-				</div><!-- /.navbar-collapse -->
-			</div><!-- /.container -->
-		</nav><!-- /#header -->
 	</header>
 	
 	<main id="main" class="container"<?php if ( isset( $navbar_position ) && 'fixed_top' === $navbar_position ) : echo ' style="padding-top: 100px;"'; elseif ( isset( $navbar_position ) && 'fixed_bottom' === $navbar_position ) : echo ' style="padding-bottom: 100px;"'; endif; ?>>
