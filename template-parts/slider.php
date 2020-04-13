8a3916a7334bcccc20bfd6f098d22bbbf42f0132
Tim <github@timgreen.ws>
2020-04-13 18:16:20 +1000

    updated slider to have ACF repeater for sliders

--- template-parts/slider.php
+++ template-parts/slider.php
@@ -1,14 +1,27 @@
-<?php 
-$images = get_field('home-slider','option');
-if( $images ): ?>
-    <div id="slider" class="slider-area">
+<div id="slider" class="slider-area">
         <div class="slick-hp-slider">
+        <?php if( have_rows('home-slider','option') ): ?>
             <div class="home-slider slides">
-                <?php foreach( $images as $image ): ?>
-                        <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
-                        <p><?php echo esc_html($image['caption']); ?></p>
-                <?php endforeach; ?>
+<?php while( have_rows('home-slider','option') ): the_row(); 
+    // slider vars
+    $slider_image = get_sub_field('slider_image','option');
+    $slider_heading = get_sub_field('slider_heading','option');
+    $slider_caption = get_sub_field('slider_caption','option');
+    $slider_button = get_sub_field('slider_button','option');
+    ?>
+   
+        <div class="slide-container">
+        <img src="<?php echo $slider_image; ?>" alt="<?php echo $image['alt'] ?>" />
+        <div class="slider-content" style="display:none">
+            <h2><?php echo $slider_heading; ?></h2>
+            <p><?php echo $slider_caption; ?></p>
+        </div>
+            <?php if( $slider_button ): ?>
+                <a href="<?php echo $slider_button; ?>"></a>
+            <?php endif; ?>
+        </div>
+    <?php endwhile; ?>
+    <?php endif; ?>
                 </div>
         </div>
     </div>
-<?php endif; ?>
